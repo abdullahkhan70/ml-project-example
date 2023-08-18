927baa6ea3180cbbca1154e8c568a21a987fca4c
Abdullah Khan <abdullahkhan9003@gmail.com>
2023-08-18 17:50:58 +0500

    Exception has been updated!

--- src/exception.py
+++ src/exception.py
@@ -18,8 +18,3 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message
         
-if __name__ == "__main__":
-    try:
-        a = 1 / 0
-    except Exception as e:
-        raise CustomException(e, sys)
